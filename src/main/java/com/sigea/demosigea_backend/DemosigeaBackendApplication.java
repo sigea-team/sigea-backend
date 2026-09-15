b5c622d48ffff2409f12b83a1281e32194521b38
JimmyDVO0823 <jimmyvelandia@outlook.es>
2026-09-15 15:12:14 -0500

    commit 1

---
+++ src/main/java/com/sigea/demosigea_backend/DemosigeaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sigea.demosigea_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosigeaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosigeaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sigea/demosigea_backend/DemosigeaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigea.demosigea_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosigeaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
